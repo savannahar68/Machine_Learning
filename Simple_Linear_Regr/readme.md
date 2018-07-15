638435e033201f875fd32e6f91181d15cd86b2b2
savannahar68 <savannahar68@gmail.com>
2018-07-15 09:47:13 +0530

    Prediction done using SLR

--- Simple_Linear_Regr/readme.md
+++ Simple_Linear_Regr/readme.md
@@ -1,6 +1,14 @@
+In this learning of Simple linear Regression, we'll build a model on Google's stock price, and then predict the
+Price of next 30 days of stock.
+We'll train our model on 80% of data and then test on 20% of data, after that we'll predict next 30 days data
+
 Requirements
+
 Pandas - sudo pip3 install pandas
 Quandl - sudo pip3 install Quandl
+numpy - sudo pip3 install numpy
+sklearn - sudo pip3 install sklearn
+matplotlib - sudo pip3 install matplotlib
 
 *If you are running on 18.04 and getting Launchpad error then run these 2 commands
 *sudo apt install python3-widgetsnbextension
--- Simple_Linear_Regr/simple_linear_regression.py
+++ Simple_Linear_Regr/simple_linear_regression.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import quandl
-import math
+import math, datetime
+import numpy as np
+from sklearn import model_selection, preprocessing, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 '''
 Quandl library Gets financial data directly
 We'll use Quandl for getting our dataset
@@ -18,11 +25,58 @@ dataFrame['PCT_Change'] = (dataFrame['Adj. Close'] - dataFrame['Adj. High'])/dat
 # Making final dataframe of the only columns which we want
 dataFrame = dataFrame[['Adj. Close' ,'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 
-forecase_col = 'Adj. Close'
+forecast_col = 'Adj. Close'
 dataFrame.fillna(-99999, inplace = True) # Handling missing data(cant use NAN)
 
-forecase_out = int(math.ceil(0.01*len(dataFrame))) # This means we'll predict 10% of data frame
-dataFrame['label'] = dataFrame[forecase_col].shift(-forecase_out) # shifting the column
+forecast_out = int(math.ceil(0.01*len(dataFrame))) # This means we'll predict 10% of data frame
+dataFrame['label'] = dataFrame[forecast_col].shift(-forecast_out) # shifting the column
 print(dataFrame.head())
 
-# Model done, training left
+# Real thing start
+
+x = np.array(dataFrame.drop(['label'], 1)) # Everything except label
+x = preprocessing.scale(x)
+x_lately = x[-forecast_out:] #for prediction
+x = x[:-forecast_out:]
+
+dataFrame.dropna(inplace = True)
+y = np.array(dataFrame['label'])
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x,y, test_size = 0.2)
+
+classifier = LinearRegression(n_jobs = 10) # To do parallelism and increase speed (Super Threading) we do n_jobs and number of process to run
+#							  n_jobs = -1 this will try to run as many jobs as possible	
+'''
+To try with SVM we will change only 1 line
+classifier = svm.SVR(kernel = 'poly') 
+It gives pretty bad accuracy
+'''
+classifier.fit(x_train, y_train)
+accuracy = classifier.score(x_test, y_test)
+
+print(accuracy)
+
+# Testing training done now forecasting and predicting 
+forecast_set = classifier.predict(x_lately)
+print(forecast_set, accuracy, forecast_out)
+
+# Plotting
+
+dataFrame['forecast'] = np.nan
+last_date= dataFrame.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400 #seconds a day
+next_unix = last_unix + one_day
+
+#For calculating date for x axis
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	dataFrame.loc[next_date] = [np.nan for _ in range(len(dataFrame.columns)-1)] + [i]
+
+dataFrame['Adj. Close'].plot()
+dataFrame['forecast'].plot()
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()	
