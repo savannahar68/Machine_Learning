476934184d4c0feaf9e2d3ccf372cd606f31a24b
savannahar68 <savannahar68@gmail.com>
2018-08-06 20:12:44 +0530

    TesorFlow mnist data prediction done

--- Neural_Networks/Mnist_NN.py
+++ Neural_Networks/Mnist_NN.py
@@ -37,27 +37,60 @@ def neural_network_model(data):
 	#purpose of biases is used during activation function, so if all the input data is all 0 then no neuron will ever fire
 	#so biase will add some value to fire a neuron in certain scenario
 	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),#initally starting with weight as 784(pixel value)
-	'biases' : tf.Variable(tf.random_normal(n_nodes_hl1))}
+	'biases' : tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
 	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-	'biases' : tf.Variable(tf.random_normal(n_nodes_hl2))}
+	'biases' : tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
 	hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-	'biases' : tf.Variable(tf.random_normal(n_nodes_hl3))}
+	'biases' : tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
 	output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
 	'biases' : tf.Variable(tf.random_normal([n_classes]))}
 
-	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
 	l1 = tf.nn.relu(l1) #rectified linear activation function
 
 
-	l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+	l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
 	l2 = tf.nn.relu(l2) #rectified linear activation function
 
-	l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+	l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
 	l3 = tf.nn.relu(l3) #rectified linear activation function
 
-	output = tf.matmul(l3, output_layer['weights']) + hidden_3_layer['biases']
+	output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
 	return output
+
+def train_neural_network(x):
+	prediction = neural_network_model(x)
+	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+
+	#now as we have cost ,we have to minize it
+	#default learning rate = 0,001
+	optimizer = tf.train.AdamOptimizer().minimize(cost) #socastic gradient decent
+	#number of cycles
+	hm_epochs = 10
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			for _ in range(int(mnist.train.num_examples/batch_size)):
+				e_x,e_y = mnist.train.next_batch(batch_size)
+				_, c = sess.run([optimizer, cost], feed_dict = {x : e_x, y : e_y})
+				epoch_loss += c
+			print('Epoch', epoch, ' Completed out of ', hm_epochs, ' loss :', epoch_loss)
+
+		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y , 1))#tf.argmax returns the highest value so we just compare
+		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+		print('accuracy :', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+
+train_neural_network(x)
+
+
+
+
+
